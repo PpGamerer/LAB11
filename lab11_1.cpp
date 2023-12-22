f61ec161af82485d8b0e5127d8cc17613051d54d
Pp <Pimolnat_ka@cmu.ac.th>
2023-12-22 16:50:49 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(){
-    cout << "Press Enter 3 times to reveal your future." << "\n";
-    cin.get() << cin.get() << cin.get();
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();cin.get();cin.get();
     srand(time(0));
     int x = rand()%9;
     if(x==0) cout << "You will get A in this 261102.";
@@ -16,6 +16,6 @@ int main(){
     else if(x==5) cout << "You will get D+ in this 261102.";
     else if(x==6) cout << "You will get D in this 261102.";
     else if(x==7) cout << "You will get F in this 261102.";
-    else if(x==8) cout << "You will get W in this 261102.";
+    else cout << "You will get W in this 261102.";
     return 0;
 }
